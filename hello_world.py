ad3fe4832e8f66bb5d35dfc52ed4bd5cb87eb92d
Piotr Jasinski <piotr.jasinski990@gmail.com>
2018-08-13 00:19:06 +0200

    general kenobi

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,13 @@
-print('hello, world!')
+x = 10
+
+
+def countdown(x):
+    if x > 0:
+        print('{}...'.format(x))
+        countdown(x-1)
+    else:
+        print("Hello there")
+
+
+if __name__ == '__main__':
+    countdown(x)
